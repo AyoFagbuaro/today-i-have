92d833a97eda4f15c021cb57020886583579b9a9
Fagbuaro Ayodeji <ayofagbuaro.dev@gmail.com>
2023-12-11 00:03:59 +0000

    Built the header component

---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+import "./Header.css";
+import HeaderOption from "./HeaderOption";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <LinkedInIcon />
+        <img src="" alt="" />
+      </div>
+
+      <div className="header__search">
+        <SearchIcon />
+        <input type="text" />
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
