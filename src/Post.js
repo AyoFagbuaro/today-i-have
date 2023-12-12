bc77421871ad32483d6261ad43e21b78ba959d5d
Fagbuaro Ayodeji <ayofagbuaro.dev@gmail.com>
2023-12-12 01:55:23 +0000

    Fleshed out the post section of the Feed Component

---
+++ src/Post.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Post.css";
+import { Avatar } from "@mui/material";
+import InputOption from "./InputOption"; // Import the InputOption component
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import CommentIcon from "@mui/icons-material/Comment";
+import ShareIcon from "@mui/icons-material/Share";
+import SendIcon from "@mui/icons-material/Send";
+import { Send } from "@mui/icons-material";
+
+function Post({ name, description, message, photoUrl }) {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar />
+        <div className="post__info">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+      <div className="post__body">
+        <p>{message}</p>
+      </div>
+      <div className="post__buttons">
+        <InputOption Icon={ThumbUpOffAltIcon} title={"Like"} />
+        <InputOption Icon={CommentIcon} title={"Comment"} />
+        <InputOption Icon={ShareIcon} title={"Share"} />
+        <InputOption Icon={Send} title={"Send"} />
+      </div>
+    </div>
+  );
+}
+
+export default Post;
